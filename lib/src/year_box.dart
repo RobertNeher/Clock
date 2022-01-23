14296ba216f647b5e031940f78ba82034a07feee
rneher <robert.neher@bartscher-hasenaecker.de>
2022-01-23 12:07:43 +0100

    Year box finished

--- lib/src/year_box.dart
+++ lib/src/year_box.dart
@@ -27,17 +27,17 @@ class _YearBoxState extends State<YearBox> {
   Widget build(BuildContext context) {
     parentSize = widget.size;
     radius = parentSize.width / 2;
-
     fontSize = radius / 16;
-    // boxSize = Size(radius / 16, fontSize + 4);
 
-    year = dateTime.toIso8601String().substring(0, 4);
+    final yearStyle = TextStyle(
+      fontFamily: 'gothicA1',
+      fontSize: fontSize,
+      fontWeight: FontWeight.w800,
+    );
+
+    final year = dateTime.toIso8601String().substring(0, 4);
 
     return Stack(alignment: Alignment.center, children: <Widget>[
-      CustomPaint(
-        size: Size(widget.size.width, widget.size.height),
-        painter: FramePainter(),
-      ),
       Container(
           width: widget.size.width - 4,
           height: widget.size.height - 4,
@@ -46,41 +46,73 @@ class _YearBoxState extends State<YearBox> {
             ' ' + year.substring(0, 2),
             textDirection: TextDirection.rtl,
             textAlign: TextAlign.end,
-            style: TextStyle(
-              fontFamily: 'gothicA1',
-              fontSize: fontSize,
-              fontWeight: FontWeight.w800,
-            ),
+            style: yearStyle,
           )),
+      CustomPaint(
+        size: Size(widget.size.width, widget.size.height),
+        painter: FramePainter(year, yearStyle),
+      ),
     ]);
   }
 }
 
 class FramePainter extends CustomPainter {
+  String year;
+  TextStyle yearStyle;
+
+  FramePainter(this.year, this.yearStyle);
+
   @override
   void paint(Canvas canvas, Size size) {
     Paint framePaint = Paint()
-      ..color = DARK_BLUE
+      ..color = Colors.blueAccent
       ..strokeCap = StrokeCap.butt
       ..style = PaintingStyle.stroke
       ..strokeWidth = 4;
+
     canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), framePaint);
+    framePaint.color = DARK_BLUE;
+    canvas.drawRect(
+        Rect.fromLTWH(-3, -3, size.width + 2, size.height + 2), framePaint);
 
-    Paint centurySeperator = Paint()
-      ..color = Colors.purple
+    Paint wheelBorder = Paint()
+      ..color = Colors.grey
       ..strokeCap = StrokeCap.butt
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 4;
+      ..strokeWidth = 2;
 
-    final Path centuryArc = Path();
-    centuryArc.moveTo(size.width / 2, size.height / 2);
-    centuryArc.arcToPoint(Offset(size.width / 2, size.height),
-        radius: Radius.circular(size.height), clockwise: true);
-    canvas.drawPath(centuryArc, centurySeperator);
+    final Path centuryWheel = Path();
+    centuryWheel.moveTo(size.width / 2.15, 0);
+    centuryWheel.arcToPoint(Offset(size.width / 2.15, size.height),
+        radius: Radius.circular(size.height), clockwise: false);
+    canvas.drawPath(centuryWheel, wheelBorder);
+
+    TextSpan decade = TextSpan(text: year[2], style: yearStyle); // Decade digit
+    TextPainter decadeText = TextPainter(
+      text: decade,
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+    );
+    decadeText.layout();
+    decadeText.paint(canvas, Offset(size.width / 2.05, 0));
+
+    final Path decadeWheel = Path();
+    decadeWheel.moveTo(size.width * 0.75, 0);
+    decadeWheel.lineTo(size.width * 0.75, size.height);
+    canvas.drawPath(decadeWheel, wheelBorder);
+
+    TextSpan year1 = TextSpan(text: year[3], style: yearStyle); // Decade digit
+    TextPainter year1Text = TextPainter(
+      text: year1,
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+    );
+    year1Text.layout();
+    year1Text.paint(canvas, Offset(size.width * 0.77, 0));
   }
 
   @override
-  bool shouldRepaint(CustomPainter olDelegate) {
+  bool shouldRepaint(CustomPainter oldDelegate) {
     return false;
   }
 }
